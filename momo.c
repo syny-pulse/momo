aca252556d801438064a8a1dd08650494710d31d
syny-pulse <sinaijose002@gmail.com>
2023-10-20 00:11:21 +0300

    Option 1 is almost ready to work

--- momo.c
+++ momo.c
@@ -2,7 +2,8 @@
 #include <conio.h>
 
 int main(void) {
-    int ch, dh, eh, fh, number, amount, balance = 5000, pin1 = 2002,pin;
+    int ch, dh, eh, fh, amount, balance = 5000, pin1 = 2002,pin;
+    char number[10];
     printf("Welcome to Pulse MoMo. \nSelect option\n");
     start:
     printf("1. Send Money \n2. Buy Airtime/Bundles \n3.Withdraw Cash \n4. Payments \n5.Financial Services \n6. Savings and loans \n7. School fees \n8. My Account\n");
@@ -17,16 +18,18 @@ int main(void) {
             scanf("%d", &amount);
             if(balance >= amount) {
                 balance = balance - amount;
-                printf("You are sending %d",amount);
-                printf(" to %s, press 1 to continue",number);
-                scanf("%d", fh);
-                if (fh == 1) {
-                        goto end;
-                        }
-                    } else {
-                        printf("Insufficient balance, load MoMo to continue\n");
-                        goto start;
-                    }
+                printf("You are sending %d to %s \n Enter 1 to continue \n",amount,number);
+                scanf("%d", eh);
+                if(eh == 1) {
+                    goto end;
+                    break;
+                } else {
+                    goto start;;
+                }
+                } else {
+                    printf("Insufficient balance, load MoMo to continue\n");
+                    goto start;
+                }
         case 2:
             break;
         case 3:
@@ -42,15 +45,14 @@ int main(void) {
         case 8:
             break;
     end:
-    printf("Enter PIN to complete transaction");
-    scanf("%d", pin);
+    printf("Enter PIN to complete the transaction: ");
+    scanf("%d", &pin); 
     if (pin == pin1) {
-        printf("Transaction completed successfully");
+        printf("Transaction completed successfully\n"); 
     } else {
-        printf("Incorrect Password, try again");
+        printf("Incorrect Password, try again\n");
         goto start;
     }
-
 
     }
 }
