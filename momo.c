87de9993ae43c7fcf5da87e4b0f2ba3f661d2847
syny-pulse <sinaijose002@gmail.com>
2023-10-21 23:55:52 +0300

    Progress, slow but sure

--- momo.c
+++ momo.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main(void) {
-    int ch, dh, eh, fh, amount, balance = 5000, pin1 = 2002,pin;
-    char number[10];
-    printf("Welcome to Pulse MoMo. \nSelect option\n");
+    int ch, dh, amount, balance = 5000, pin1 = 2002, pin;
+    char number[11]; 
+
+    printf("Welcome to Pulse MoMo. Select an option\n");
     start:
-    printf("1. Send Money \n2. Buy Airtime/Bundles \n3.Withdraw Cash \n4. Payments \n5.Financial Services \n6. Savings and loans \n7. School fees \n8. My Account\n");
+
+    printf("1. Send Money\n2. Buy Airtime/Bundles\n3. Withdraw Cash\n4. Payments\n5. Financial Services\n6. Savings and loans\n7. School fees\n8. My Account\n");
     scanf("%d", &ch);
+
     switch(ch) {
         case 1:
-            printf("1. Send to Airtel \n2. Send to other network\n");
-            scanf("%d", dh);
-            printf("Enter number to send to \n");
-            scanf("%s", &number);
-            printf("Enter amount \n");
+            printf("1. Send to Airtel\n2. Send to other network\n");
+            scanf("%d", &dh);  
+            printf("Enter number to send to: ");
+            scanf("%s", number);
+            printf("Enter amount: ");
             scanf("%d", &amount);
-            if(balance >= amount) {
+            if (balance >= amount) {
                 balance = balance - amount;
-                printf("You are sending %d to %s \n Enter 1 to continue \n",amount,number);
-                scanf("%d", eh);
-                if(eh == 1) {
-                    goto end;
-                    break;
-                } else {
-                    goto start;;
-                }
-                } else {
-                    printf("Insufficient balance, load MoMo to continue\n");
-                    goto start;
-                }
+                printf("You are sending %d to %s\nEnter your PIN to complete the transaction: ", amount, number);
+                scanf("%d", &pin);
+                goto end;
+            break;
         case 2:
+            printf("1. Buy for self\n2. Buy for another number\n");
+            scanf("%d", &dh);  
+            printf("Enter number to send to: ");
+            scanf("%s", number);
+            printf("Enter amount: ");
+            scanf("%d", &amount);
+            if (balance >= amount) {
+                balance = balance - amount;
+                printf("You are sending %d to %s\nEnter your PIN to complete the transaction: ", amount, number);
+                scanf("%d", &pin);
+                goto end;
+            break;
             break;
         case 3:
             break;
@@ -45,8 +51,6 @@ int main(void) {
         case 8:
             break;
     end:
-    printf("Enter PIN to complete the transaction: ");
-    scanf("%d", &pin); 
     if (pin == pin1) {
         printf("Transaction completed successfully\n"); 
     } else {
